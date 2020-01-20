669fb3f32fb67066f90df0e8ad799a46e985748c
Aliya Safina <ktotamy@gmail.com>
2020-01-20 16:43:46 +0300

    move file saver from module to app

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -3,6 +3,7 @@ import '../css/menu-style.css';
 import '../css/context-menu-style.css';
 import '../../favicon.ico';
 import * as $ from 'jquery';
+import { saveAs } from "file-saver";
 
 import MindMapRender from 'mm-render/src/mm-render';
 
@@ -11,6 +12,7 @@ const mmRender = new MindMapRender();
 const btnNew = document.getElementById('new');
 const btnOpen = document.getElementById('open');
 const btnSave = document.getElementById('save');
+const btnSaveAs = document.getElementById('saveAs');
 const btnExpandAll = document.getElementById('expandAll');
 const btnCollapseAll = document.getElementById('collapseAll');
 const btnFocusRoot = document.getElementById('focusRoot');
@@ -24,7 +26,15 @@ btnNew.addEventListener('click', () => {
 
 btnSave.addEventListener('click', () => {
     if (isOpen) {
-        mmRender.saveJson();
+        saveAsJson();
+    } else {
+        alert('No thing to save!');
+    }
+});
+
+btnSaveAs.addEventListener('click', () => {
+    if (isOpen) {
+        saveAsJson();
     } else {
         alert('No thing to save!');
     }
@@ -66,4 +76,9 @@ function toggleOpen() {
     document.querySelectorAll('.toggle').forEach((elem) => {
         elem.style.display = 'block';
     });
+}
+
+function saveAsJson() {
+    let blob = new Blob([mmRender.getTreeData()], {type: "application/json"});
+    saveAs(blob, `${mmRender.fileName}.json`);
 }
